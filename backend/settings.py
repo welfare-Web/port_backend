5827c0ebfe7cea914ddbaf2cf19a98584d0cef70
welfare-Web <tlepubwelfare@gmail.com>
2026-01-04 14:03:33 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -169,15 +169,7 @@ SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 
 
-# --------------------------------------------------
-# EMAIL CONFIGURATION (SAFE)
-# --------------------------------------------------
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
 
 
 
